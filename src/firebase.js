0a4245ecf784e0a86032652a7bf602433ece8856
Ashmeet <ashmeetsingh152@gmail.com>
2022-10-11 16:25:07 +0530

    resume builder

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+// Initialize Firebase
+let fbObj = {
+//  ur key from firebase
+}
+firebase.initializeApp(fbObj);
+export default firebase;
